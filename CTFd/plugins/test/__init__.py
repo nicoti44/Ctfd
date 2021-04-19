6b15f3d79ef8167140eb2880d1311b4a79f023bb
Charles HUET <charles.huet@ynov.com>
2021-04-20 00:26:02 +0200

    eho nicoals

--- CTFd/plugins/test/__init__.py
+++ CTFd/plugins/test/__init__.py
@@ -4,11 +4,8 @@ from CTFd.plugins import register_plugin_assets_directory
 from flask import render_template
 
 
-
 def test():
     return True
-
-
 
 
 class BaseTest(object):
@@ -36,13 +33,14 @@ class CTFdTest(BaseFlag):
 FLAG_CLASSES = {"static": CTFdStaticFlag, "regex": CTFdRegexFlag}
 
 
-
 def load(app):
- @app.route('/test', methods=['GET'])
+    @app.route('/test', methods=['GET'])
+
     def view_faq():
-        return render_template('static/page.html', content="<h1>COUCOU NICOLAS VOILA UN BOUTON</h1><button onclick="alert('jui un haxer')">eho ami</button>")
+        return render_template('static/page.html', content="<h1>COUCOU NICOLAS VOILA UN BOUTON</h1><button onclick="
+        alert('jui un haxer')
+        ">eho ami</button>")
 
 
 
-    register_plugin_assets_directory(app, base_path="/plugins/flags/static/")
-
+        register_plugin_assets_directory(app, base_path="/plugins/flags/static/")
